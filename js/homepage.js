0e97c9abe883595f5797d3903bd46dfed86872df
FCX <fabrice.coitoux@orange.fr>
2021-04-03 15:42:55 +0200

    js homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,6 @@
 //===================================================================
 //variables
 
-let urlList = ["http://localhost:3000/api/teddies", "http://localhost:3000/api/cameras", "http://localhost:3000/api/furniture"];
-
-let groupList = ["peluches", "photo", "meubles"];
-
 let basketTotalItems = Number(localStorage.getItem("basketItems"));
 // let basketTotalItems = 0;
 
@@ -17,25 +13,18 @@ if (basketTotalItems !== 0) {
     document.getElementById('buttonBasket').insertAdjacentHTML('beforeend', '<a class="nav-link" href="./html/basket.html"><i class="fas fa-shopping-basket mr-1"></i>Panier<span class="badge bg-secondary">(vide)</span></a>');
 }
 
-document.getElementById('test').insertAdjacentHTML('beforebegin', '<h2>' + basketTotalItems + '</h2');
+// document.getElementById('test').insertAdjacentHTML('beforebegin', '<h2>' + basketTotalItems + '</h2');
 
 //====================================================================
 //catégories
 
-function buildCategory(url, group) {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            var response = JSON.parse(this.responseText);
-            document.getElementById('category').insertAdjacentHTML("afterbegin", '<div class="col"><div class="card h-100"><div class="card-body"><h3 id="category_title" class="card-title text-center text-capitalize">' + group + '</h3></div><img id="category_img" src="' + response[1].imageUrl + '" class="card-img-top img-thumbnail" alt="..."><div class="card-footer text-center"><a href="#' + group + '"class="btn btn-light stretched-link"><h4 class="font-weight-bold"">Consulter</h4></a></div></div></div> ');
-        }
-    };
-    request.open("GET", url);
-    request.send();
-}
-
 for (let i = 0; i < urlList.length; i++) {
-    buildCategory(urlList[i], groupList[i]);
+    getDatas(urlList[i], groupList[i]).then((response) => {
+        document.getElementById('category_title').insertAdjacentHTML('beforeend', '<h3 class="card-title text-center text-capitalize">' + groupList[i] + '</h3>');
+        document.getElementById('category_img').src = response[1].imageUrl;
+        document.getElementById('category_img').alt = response[1].name;
+        document.getElementById('category_a').href = '#' + groupList[i];
+    });
 }
 
 //====================================================================
@@ -47,7 +36,7 @@ function buildCatalogue(url, group) {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             var response = JSON.parse(this.responseText);
             for (elt of response) {
-                document.getElementById(group).insertAdjacentHTML("beforeend", '<div class="card mb-3" style="max-width: 550px;"><div class="row g-0"><div class="col-md-6"><img src="' + elt.imageUrl + '" class="d-block w-100 img-thumbnail" alt="..."></div><div class="col-md-6"><div class="card-body"><h5 class="card-title">' + elt.name + '</h5><p class="card-text">' + elt.price + ' €</p><a href="./html/product.html" class="btn btn-primary stretched-link">En savoir +</a></div></div></div></div>');
+                document.getElementById(group).insertAdjacentHTML("beforeend", '<div class="col-6 card mb-3" style="max-width: 550px;"><div class="row g-0"><div class="col-md-6"><img src="' + elt.imageUrl + '" class="d-block w-100 img-thumbnail" alt="..."></div><div class="col-md-6"><div class="card-body"><h5 class="card-title">' + elt.name + '</h5><p class="card-text">' + elt.price + ' €</p><a href="./html/product.html" class="btn btn-primary stretched-link">En savoir +</a></div></div></div></div>');
             }
         }
     };
@@ -56,7 +45,7 @@ function buildCatalogue(url, group) {
 }
 
 for (let i = 0; i < urlList.length; i++) {
-    document.getElementById("catalogue").insertAdjacentHTML("beforeend", '<h3 id="' + groupList[i] + '" class="font-weight-bold font-italic mt-5 mb-3 text-capitalize">' + groupList[i] + '</h3> <div class="d-flex justify-content-around flex-wrap" >');
+    document.getElementById("catalogue").insertAdjacentHTML("beforeend", '<h3 class="font-weight-bold font-italic mt-5 mb-3 text-capitalize">' + groupList[i] + '</h3><div id="' + groupList[i] + '" class="row">');
 
     buildCatalogue(urlList[i], groupList[i]);
 
